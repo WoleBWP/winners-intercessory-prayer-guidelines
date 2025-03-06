b675bc7bb0520294c0c5e3511a8933e45498aeff
WoleBWP <151994709+WoleBWP@users.noreply.github.com>
2025-03-06 13:33:34 +0100

    Add files via upload

---
+++ Supernatural Church Growth/supernatural-church-growth.js
@@ -0,0 +1,101 @@
+let prayerGuideline = [
+  {
+    point: `Father, in the name of Jesus, thank You for Your mighty hand that is behind the continuous growth of this Church till date.`,
+    scripture: `John 6:44 (KJV) No man can come to me, except the Father which hath sent me draw him: and I will raise him up at the last day.`,
+  },
+  {
+    point: `Father, in the name of Jesus, thank You for supernatural increases that we have continued to experience in this church over the years.`,
+    scripture: `1 Corinthians 3:6 (KJV) I have planted, Apollos watered; but God gave the increase.`
+  },
+  {
+    point: `Father, in the name of Jesus, thank You for Your divine visitation in our midst over the years, resulting in signs, wonders and divers' miracles`,
+    scripture: `Zephaniah 3:17 (KJV) The LORD thy God in the midst of thee is mighty; he will save, he will rejoice over thee with joy; he will rest in his love, he will joy over thee with singing.`
+  },
+  {
+    point: `Father, in the name of Jesus, thank You for destroying all interferences of the devil against the continuous growth of this Church over the years.`,
+    scripture: `Revelation 12:11 (KJV) And they overcame him by the blood of the Lamb, and by the word of their testimony; and they loved not their lives unto the death.`
+  },
+  {
+    point: `Father, in the name of Jesus, cause Your acts in this Church to be noised abroad as in the day of Pentecost, thereby drafting abiding multitudes into this Church all through this year.`,
+    scripture: `Acts 2:6 (KJV) Now when this was noised abroad, the multitude came together, and were confounded, because that every man heard them speak in his own language. <br><br>Acts 2:41 (KJV) Then they that gladly received his word were baptized: and the same day there were added unto them about three thousand souls.`
+  },
+  {
+    point: `Father, in the name of Jesus, let every captive ordained for eternal life across our harvest field this year be released, saved and established in this Church.`,
+    scripture: `Acts 13:48 (KJV) And when the Gentiles heard this, they were glad, and glorified the word of the Lord: and as many as were ordained to eternal life believed.`
+  },
+  {
+    point: `Father, in the name of Jesus, we cast out every evil beast seeking to manipulate our new converts and new members from being established in this Church all through this year.`,
+    scripture: `Ezekiel 34:25 (KJV) And I will make with them a covenant of peace, and will cause the evil beasts to cease out of the land: and they shall dwell safely in the wilderness, and sleep in the woods.`
+  },
+  {
+    point: `Father, in the name of Jesus, let this Church continue to experience supernatural growth after the order of the Acts of the Apostles; thereby turning her into a city without walls all through this year.`,
+    scripture: `Acts 13:44 (KJV) And the next sabbath day came almost the whole city together to hear the word of God.`
+  },
+  {
+    point: `Father, in the name of Jesus, we destroy the gates of hell that may resist the continuous influx of souls into this Church all through the year.`,
+    scripture: `Matthew 16:18 (KJV) And I say also unto thee, That thou art Peter, and upon this rock I will build my church; and the gates of hell shall not prevail against it.`
+  },
+  {
+    point: `Father, in the name of Jesus, empower every Cell in this Church for supernatural growth; resulting in further replication of cells all through this year.`,
+    scripture: `Jeremiah 30:19 (KJV) And out of them shall proceed thanksgiving and the voice of them that make merry: and I will multiply them, and they shall not be few; I will also glorify them, and they shall not be small.`
+  },
+  {
+    point: `Father, in the name of Jesus, turn every cell into a solution-centre where every member's needs shall continue to be supernaturally met all through this year.`,
+    scripture: `Zephaniah 3:17 (KJV) The LORD thy God in the midst of thee is mighty; he will save, he will rejoice over thee with joy; he will rest in his love, he will joy over thee with singing.`
+  },
+  {
+    point: `Father, in the name of Jesus, let every cell minister be empowered by the Holy Ghost to effectively drive the Cell Replication Agenda of the year.`,
+    scripture: `Acts 10:38 (KJV) How God anointed Jesus of Nazareth with the Holy Ghost and with power: who went about doing good, and healing all that were oppressed of the devil; for God was with him.`
+  },
+  {
+    point: `Father, in the name of Jesus, let there be a continuous increase of the Word from our altar all through the year, leading to the supernatural growth of this Church.`,
+    scripture: `Acts 6:7 (KJV) And the word of God increased; and the number of the disciples multiplied in Jerusalem greatly; and a great company of the priests were obedient to the faith.`
+  },
+  {
+    point: `Father, in the name of Jesus, we decree the liberty of all our new converts from all forms of bondages, spells, enchantments and generational curses, so they can abide in this Church for life.`,
+    scripture: `Numbers 23:23 (KJV) Surely there is no enchantment against Jacob, neither is there any divination against Israel: according to this time it shall be said of Jacob and of Israel, What hath God wrought!`
+  },
+  {
+    point: `Father, in the name of Jesus, give all our new converts and new members in this Church a testimony of once I was blind, now I can see'', so they can be established in the faith and in this Church`,
+    scripture: `John 9:25 (KJV) He answered and said, Whether he be a sinner or no, I know not: one thing I know, that, whereas I was blind, now I see.`
+  },
+  {
+    point: `Father, in the name of Jesus, release Your reaperangels to wait upon all our new converts and every challenged Winner, mobilizing them to appear in this Church next Sunday, for their restoration and breakthroughs.`,
+    scripture: `Matthew 26:53 (KJV) Thinkest thou that I cannot now pray to my Father, and he shall presently give me more than twelve legions of angels?`
+  },
+  {
+    point: `Father, in the name of Jesus, let the Holy Ghost come down as a 'Mighty, Rushing Wind', drafting unprecedented multitudes into this Church next Sunday.`,
+    scripture: `Numbers 11:31 (KJV) And there went forth a wind from the LORD, and brought quails from the sea, and let them fall by the camp, as it were a day's journey on this side, and as it were a day's journey on the other side, round about the camp, and as it were two cubits high upon the face of the earth.`
+  },
+  {
+    point: `Father, in the name of Jesus, renew the apostolic seal upon Your servant, the Apostle over this Commission, resulting in signs and wonders and mighty-deeds amongst the people.`,
+    scripture: `2 Corinthians 12:12 (KJV) Truly the signs of an apostle were wrought among you in all patience, in signs, and wonders, and mighty deeds.`
+  },
+  {
+    point: `Father, in the name of Jesus, endue our pastors at all levels, with wisdom from above, resulting in the continuous growth and expansion of this Church.`,
+    scripture: `Isaiah 33:6 (KJV) And wisdom and knowledge shall be the stability of thy times, and strength of salvation: the fear of the LORD is his treasure.`
+  },
+  {
+    point: `Father, in the name of Jesus, continue to endue the Apostle over this Commission with divine wisdom, in driving the continuous growth of this Church.`,
+    scripture: `Ecclesiastes 10:10 (KJV) If the iron be blunt, and he do not whet the edge, then must he put to more strength: but wisdom is profitable to direct.`
+  }
+];
+
+let prayerGuidelineHTML = "";
+
+prayerGuideline.forEach((prayerPoint) => {
+  prayerGuidelineHTML += `
+     
+  <li>
+    <div>
+        <p class = "prayer-point">${prayerPoint.point}</p>
+        <p class = "scripture" style="color: green;">${prayerPoint.scripture}</p>
+    </div>
+  </li>
+        
+
+    `;
+});
+
+document.querySelector(".js-prayer-point").innerHTML = prayerGuidelineHTML;
+
---
+++ Towards Impactful Soulwinner Outreaches/towards-impactful -soulwinner-outreaches.js
@@ -0,0 +1,113 @@
+let prayerGuideline = [
+  {
+    prayer: `1.`,
+    point: `Father, in the name of Jesus, we decree that every gate of hell be shattered as Winners go out for outreaches all through this year; thereby leading many to Christ across our harvest field.`,
+    scripture: `(Php 2:9)  Wherefore God also hath highly exalted him, and given him a name which is above every name:<br><br>
+                (Php 2:10)  That at the name of Jesus every knee should bow, of things in heaven, and things in earth, and things under the earth;<br><br>
+                (Php 2:11)  And that every tongue should confess that Jesus Christ is Lord, to the glory of God the Father.`
+  },
+  {
+    point: `Father, in the name of Jesus, we decree that the veil over the hearts of men be torn off across our harvest field all through this year; thereby leading many to salvation.`,
+    scripture: `(2Co 3:14)  But their minds were blinded: for until this day remaineth the same vail untaken away in the reading of the old testament; which vail is done away in Christ.`
+  },
+  {
+    point: `Father, in the name of Jesus, we decree the destruction of all the powers of darkness out to resist the entrance of the Gospel, as Winners march across our harvest field all through this year.`,
+    scripture: `(Joh 1:5)  And the light shineth in darkness; and the darkness comprehended it not.`
+  },
+  {
+    point: `Father, in the name of Jesus, open the heart of every contact on the harvest field all through this year to every word spoken; thereby leading many to Christ.`,
+    scripture: `(Act 16:14)  And a certain woman named Lydia, a seller of purple, of the city of Thyatira, which worshipped God, heard us: whose heart the Lord opened, that she attended unto the things which were spoken of Paul.
+`
+  },
+  {
+    point: `Father, in the name of Jesus, turn everyone on the field all through this year into a sharp threshing instruments for effectiveness in reaping the harvest.`,
+    scripture: `(Isa 41:15)  Behold, I will make thee a new sharp threshing instrument having teeth: thou shalt thresh the mountains, and beat them small, and shalt make the hills as chaff.
+`
+  },
+  {
+    point: `Father, in the name of Jesus, grant supernatural utterance to every soulwinner all through this year, leading to massive salvation and ingathering of souls into this Church.`,
+    scripture: `(2Th 3:1)  Finally, brethren, pray for us, that the word of the Lord may have free course, and be glorified, even as it is with you:
+`
+  },
+  {
+    point: `Father, in the name of Jesus, grant supernatural wisdom to every soul winner all through this year; thereby turning many to righteousness.`,
+    scripture: `(Dan 12:3)  And they that be wise shall shine as the brightness of the firmament; and they that turn many to righteousness as the stars for ever and ever.`
+  },
+  {
+    point: `Father, in the name of Jesus, give us the heathen for our inheritance and the uttermost part of the earth for our possession all through this year.`,
+    scripture: `(Psa 2:8)  Ask of me, and I shall give thee the heathen for thine inheritance, and the uttermost parts of the earth for thy possession.`
+  },
+  {
+    point: `Father, in the name of Jesus and by the power of the Holy Ghost, grant supernatural boldness to every soul winner across our harvest field all through this year that will result in signs and wonders, thereby leading many to salvation.`,
+    scripture: `(Act 14:3)  Long time therefore abode they speaking boldly in the Lord, which gave testimony unto the word of his grace, and granted signs and wonders to be done by their hands.`
+  },
+  {
+    point: `Father, in the name of Jesus, send Your reaper-angels to take over our harvest field all through this year, thereby drafting multitudes into this Church for their salvation, deliverance and breakthroughs.`,
+    scripture: `(Mat 13:39)  The enemy that sowed them is the devil; the harvest is the end of the world; and the reapers are the angels.`
+  },
+  {
+    point: `Father, in the name of Jesus, let everyone in possession of our tracts and fliers, be compelled to be in Church for their salvation and establishment in the faith all through this year.`,
+    scripture: `(Exo 23:20)  Behold, I send an Angel before thee, to keep thee in the way, and to bring thee into the place which I have prepared.`
+  },
+  {
+    point: `Father, in the name of Jesus, let the Holy Spirit breathe upon our tracts and fliers all through this year, turning them into anointed sickles of harvest, thereby drafting multitudes into this Church.`,
+    scripture: `(Joe 3:13)  Put ye in the sickle, for the harvest is ripe: come, get you down; for the press is full, the fats overflow; for their wickedness is great.`
+  },
+  {
+    point: `Father, in the name of Jesus, let the Holy Spirit continue to 'Whistle' across our harvest field; thereby compelling the ingathering of abiding multitudes into this Church all through this year.`,
+    scripture: `(Isa 5:26)  And he will lift up an ensign to the nations from far, and will hiss unto them from the end of the earth: and, behold, they shall come with speed swiftly:`
+  },
+  {
+    point: `Father, in the name of Jesus, let the Holy Ghost take over our harvest field, convicting and converting multitudes as we reach out all through this year.`,
+    scripture: `(Joh 16:8)  And when he is come, he will reprove the world of sin, and of righteousness, and of judgment: <br><br>
+                (Joh 16:9)  Of sin, because they believe not on me; <br><br>
+                (Joh 16:10)  Of righteousness, because I go to my Father, and ye see me no more; <br><br>
+                (Joh 16:11)  Of judgment, because the prince of this world is judged.`
+  },
+  {
+    point: `Father, in the name of Jesus, we decree the release of reaper-angels to the highways and hedges, compelling the influx of abiding multitudes into this Church all through this year.`,
+    scripture: `(Mat 13:39)  The enemy that sowed them is the devil; the harvest is the end of the world; and the reapers are the angels.`
+  },
+  {
+    point: `Father, in the name of Jesus, let the Holy Ghost come down as a 'Mighty, Rushing Wind, drafting abiding multitudes into this Church this year.`,
+    scripture: `(Num 11:31)  And there went forth a wind from the LORD, and brought quails from the sea, and let them fall by the camp, as it were a day's journey on this side, and as it were a day's journey on the other side, round about the camp, and as it were two cubits high upon the face of the earth.
+`
+  },
+  {
+    point: `Father, in the name of Jesus, let the ears of every unsaved soul across our harvest field, continue to hear the compelling sound of the Holy Spirit all through this year and be drafted into this Church.`,
+    scripture: `(Isa 5:26)  And he will lift up an ensign to the nations from far, and will hiss unto them from the end of the earth: and, behold, they shall come with speed swiftly:`
+  },
+  {
+    point: ` Father, in the name of Jesus and by the Blood of the Lamb, we decree hitch-free movement for all worshippers in and out of Church all through this year.`,
+    scripture: `(Psa 103:13)  Like as a father pitieth his children, so the LORD pitieth them that fear him. <br><br>
+                (Psa 103:14)  For he knoweth our frame; he remembereth that we are dust. <br><br>
+                (Psa 103:15)  As for man, his days are as grass: as a flower of the field, so he flourisheth.`
+  },
+  {
+    point: `Father, in the name of Jesus, go before us in all our outreaches this year and make the crooked path straight as we reach out to bring the lost to Christ.`,
+    scripture: `(Isa 45:1)  Thus saith the LORD to his anointed, to Cyrus, whose right hand I have holden, to subdue nations before him; and I will loose the loins of kings, to open before him the two leaved gates; and the gates shall not be shut; <br><br>
+                (Isa 45:2)  I will go before thee, and make the crooked places straight: I will break in pieces the gates of brass, and cut in sunder the bars of iron:`
+  },
+  {
+    point: `Father, in the name of Jesus, appear to every worshipper in all our services through Your Word for their supernatural turnarounds all though this year, thereby drafting abiding multitudes into this Church.`,
+    scripture: `(Act 13:44)  And the next sabbath day came almost the whole city together to hear the word of God.`
+  }
+];
+
+let prayerGuidelineHTML = "";
+
+prayerGuideline.forEach((prayerPoint) => {
+  prayerGuidelineHTML += `
+    <li> 
+      <div>
+          <p class = "prayer-point">${prayerPoint.point}</p>
+          <p class = "scripture" style="color: red;">${prayerPoint.scripture}</p>
+      </div>     
+    </li>
+    `;
+});
+
+
+document.querySelector(".js-prayer-point").innerHTML = prayerGuidelineHTML;
+
+
